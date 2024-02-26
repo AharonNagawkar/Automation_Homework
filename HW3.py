3f829daec0b676e7b4b54d5f28865baf003d190b
Aharonn <nagaukar@gmail.com>
2024-02-26 20:27:35 +0200

    HW3.py question3

--- HW3.py
+++ HW3.py
@@ -10,10 +10,26 @@ my_info = {
 def change_list(my_list):
     try:
         my_list[4] = '@'
-        return my_list
     except IndexError as msg:
         print(msg)
+    else:
+        return my_list
 
 
-print(change_list([1, 2, 3, 4]))
-print(change_list([1, 2, 3, 4, 5]))
+# print(change_list([1, 2, 3, 4]))
+# print(change_list([1, 2, 3, 4, 5]))
+
+
+# Question 3:
+def change_list2(my_list2):
+    try:
+        assert len(my_list2) > 4, 'The list is too short'
+    except AssertionError as msg:
+        print(msg)
+    else:
+        my_list2[4] = '@'
+        return my_list2
+
+
+print(change_list2([1, 2, 3, 4]))
+print(change_list2([1, 2, 3, 4, 5]))
